f1dda5171df39cfdd9a570828128e1d16485c85b
Susan Shilbi <76254590+susan7shilbi@users.noreply.github.com>
2023-11-21 18:36:24 +0530

    1st commit

---
+++ src/main/java/labfat/App.java
@@ -0,0 +1,7 @@
+package labfat;
+
+public class App {
+	public static void main(String[] args) {
+			System.out.println("Hello World");
+	}
+}
---
+++ src/test/java/labfat/AppTest.java
@@ -0,0 +1,5 @@
+package labfat;
+
+public class AppTest {
+
+}
